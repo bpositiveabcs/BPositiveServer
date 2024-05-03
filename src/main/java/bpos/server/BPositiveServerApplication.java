ee9d5e7d8afcfd3ebc4c7d5a62fd32d795d914ec
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-03 23:49:05 +0300

    first commit

---
+++ src/main/java/bpos/server/BPositiveServerApplication.java
@@ -0,0 +1,13 @@
+package bpos.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BPositiveServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BPositiveServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bpos/server/BPositiveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bpos.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BPositiveServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
