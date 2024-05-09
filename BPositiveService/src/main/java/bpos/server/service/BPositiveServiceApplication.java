8f0f9b6828d162908b3c033209dcdda1e93d7b13
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-09 17:59:15 +0300

    now it wont have any problems trying to find datasource

--- BPositiveService/src/main/java/bpos/server/service/BPositiveServiceApplication.java
+++ BPositiveService/src/main/java/bpos/server/service/BPositiveServiceApplication.java
@@ -1,9 +1,12 @@
 package bpos.server.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class BPositiveServiceApplication {
 
     public static void main(String[] args) {
