9506107177aa691811b312bddb08e25e1924c8e1
Deea <116511056+deeaion@users.noreply.github.com>
2024-06-01 22:09:34 +0300

    nu mai crapa yey

--- BPositiveService/src/main/java/bpos/server/service/WebSockets/SecurityConfig.java
+++ BPositiveService/src/main/java/bpos/server/service/WebSockets/SecurityConfig.java
@@ -1,20 +1,24 @@
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/client-websocket/**").permitAll() // Allow access to WebSocket endpoints
-//                .anyRequest().authenticated()
-//                .and()
-//                .csrf().disable() // Disable CSRF protection for WebSocket connections
-//                .headers().frameOptions().sameOrigin(); // Allow same origin for frame options
-//    }
-//}
+package bpos.server.service.WebSockets;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/client-websocket/**").permitAll() // Allow access to WebSocket endpoints
+                                .anyRequest().permitAll() // Allow access to all other requests
+                )
+                .csrf().disable(); // Disable CSRF protection for WebSocket connections
+        return http.build();
+    }
+}
